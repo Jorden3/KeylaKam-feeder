ae3c137328a80f4993e711d2d55eb3a187331171
jorden3 <jorden3@pdx.edu>
2018-08-12 19:06:35 -0700

    Feeder is streaming images.

--- feeder.js
+++ feeder.js
@@ -1,11 +1,11 @@
 var io = require('socket.io-client');
-var ss = require('socket.io-stream');
+//var ss = require('socket.io-stream');
 var fs = require('fs');
 
 
 var spawn = require('child_process').spawn;
  
-var socket = io.connect('http://localhost:8080', {reconnect: true});
+var socket = io.connect('http://192.168.1.7:8080', {reconnect: true});
 //var stream = ss.createStream();
 //var filename = 'profile.jpg';
 
@@ -14,28 +14,23 @@ startStreaming();
 
 function stopStreaming() {
     if (Object.keys(sockets).length == 0) {
-      app.set('watchingFile', false);
       if (proc) proc.kill();
       fs.unwatchFile('./stream/image_stream.jpg');
     }
   }
 
 function startStreaming() {
- 
-    if (app.get('watchingFile')) {
-      socket.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
-      return;
-    }
    
-    var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
+    var args = ["-w", "640", "-h", "480", "-o", "/home/pi/Desktop/image_stream.jpg","-n","-t", "999999999", "-tl", "100"];
     proc = spawn('raspistill', args);
    
     console.log('Watching for changes...');
    
-    app.set('watchingFile', true);
-   
-    fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
-      io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+    fs.watchFile('/home/pi/Desktop/image_stream.jpg', function(current, previous) {
+      fs.readFile('/home/pi/Desktop/image_stream.jpg', function(err, buff){
+        socket.emit('liveStream', buff);
+      });
+      
     })
    
   }
